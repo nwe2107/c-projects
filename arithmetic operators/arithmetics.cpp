fbab5236a1e6a95a1ffa7e8f6509bad3d6f03f3b
eriko <aripr6@gmail.com>
2026-04-03 21:24:23 +0300

    studied arithmetic operators in c++

---
+++ arithmetic operators/arithmetics.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+    // arithmetic operators =   return the result of a specific 
+    //                          arithmetic operation ( + - * / )
+
+    //int students = 20 ; 
+
+    // students = students + 1; 
+    // students -= 1;
+    // students--;
+    // students *= 2;
+    // students /= 2;
+    // students %= 2;
+
+    // order of operations
+    // 1. parenthesis
+    // 2. multiplication and division
+    // 3. addition and subtraction
+    // 4. left to right
+    int students = 6 - 5 + 4 * 3 / 2;
+
+
+    std::cout <<students;
+    return 0;
+}
